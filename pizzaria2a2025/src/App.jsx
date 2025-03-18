c592381adca2e73ef8714467b970a00acd6b06af
DouglasLuz09 <odoug2009@gmail.com>
2025-03-18 08:28:08 -0300

    componente

--- pizzaria2a2025/src/App.jsx
+++ pizzaria2a2025/src/App.jsx
@@ -1,22 +1,33 @@
 import {useState} from 'react'
+ 
 function App(){
- 
-
-const [nome, setNome] = useState('Ronaldo')
-  return (
- 
-  <div>
-    <h3>Pizzaria 2A</h3>
+ const [nome, setNome] = useState('Ronaldo');
+ const Formulario =() => {
+   return (
+<>
+    <h4>Formulário de Inscrição</h4>
     <input className="Nome"
     name = "nome"
-    placeholder="digite seu nome..."
-     onChange={(e)=>setNome(e.target.value)}
-     type="text"/>
-      <button onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
-  </div>
+    placeholder="Digite seu nome..."
+    onChange={(e)=>setNome(e.target.value)}
+      type="text" />
+    <button onClick={()=>{alert(nome)}}>
+      CLIQUE AQUI
+      </button>  
+   
+</>
+)
+ }
+ 
+  return (
+    <div>
+      <h3>Pizzaria 2A</h3>
+     <Formulario />
+     <Formulario />
+     <Formulario />
+    </div>
   )
 }
  
 export default App
+ 
